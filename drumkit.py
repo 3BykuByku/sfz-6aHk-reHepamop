2c93c322541d715fb7e1d978462ce02c0f05b790
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2020-01-11 13:04:37 +0300

    fix

--- drumkit.py
+++ drumkit.py
@@ -1,11 +1,13 @@
-import sys, os, csv, re
-from os import scandir, walk, getcwd
+import os
+import re
+import sys
+from collections import OrderedDict
+from os import walk, getcwd
 from os.path import dirname
-from collections import namedtuple, defaultdict, OrderedDict
 
 import GM
 
-if (len(sys.argv) > 1):
+if len(sys.argv) > 1:
 	samples_path = sys.argv[1]
 else:
 	samples_path = getcwd()
@@ -14,21 +16,21 @@ pattern_order = re.compile("(\d+)\.")
 
 # init samples by folder names and order in file names
 samples = {}
-for root, dirs, files in os.walk(samples_path):
+for root, dirs, files in walk(samples_path):
 	for name in files:
-		if (".sfz" in name):
+		if ".sfz" in name:
 			continue
 		fname = os.path.join(root, name).replace(samples_path, "")[1:]
 		patch_name = dirname(fname)
-		if not patch_name in GM.drums:
+		if patch_name not in GM.drums:
 			print("Error - patch name: " + patch_name)
 			continue
 		tmp = pattern_order.search(fname)
 		if tmp.group(1):
 			order = tmp.group(1)
-			if not patch_name in samples:
+			if patch_name not in samples:
 				samples[patch_name] = OrderedDict()
-			if not order in samples[patch_name]:
+			if order not in samples[patch_name]:
 				samples[patch_name][order] = []
 			samples[patch_name][order].append(fname)
 		else:
@@ -41,20 +43,18 @@ patch_template = """<region> lovel={level_min} hivel={level_max} sample={sample_
 
 with open(samples_path + "/generated.sfz", "w") as fout:
 	for patch_name in samples:
-		#print(patch_name)
-		fout.write("\n// " + GM.drums[patch_name].title + "\n")
+		fout.write("\n// " + GM.drums[patch_name][2] + ' | ' + GM.drums[patch_name][1] + ' | ' + GM.drums[patch_name][3] + "\n")
 
 		incr = 128 // len(samples[patch_name])
 		level_min = 0
 		i = 1
 		for order in samples[patch_name]:
-			#print(samples[patch_name][order])
 			j = 1
 			size = len(samples[patch_name][order])
 			for sample in samples[patch_name][order]:
-				#print([level_min, level_min + incr, sample, j])
+				# ('code', 'note', 'title', 'pattern')
 				data = {
-					"code": GM.drums[patch_name].code,
+					"code": GM.drums[patch_name][0],
 					"level_min": level_min,
 					"level_max": 127 if i == len(samples[patch_name]) else level_min + incr - 1,
 					"sample_path": sample,
--- update-gm-drums.py
+++ update-gm-drums.py
@@ -1,20 +1,15 @@
 import csv
-from collections import namedtuple
 
 # drum patches info
 #  https://en.wikipedia.org/wiki/General_MIDI#Percussion
 #  http://computermusicresource.com/GM.Percussion.KeyMap.html
-
-drum = namedtuple('drum', ['code', 'note', 'title', 'pattern'])
 
 drums = {}
 with open('GM.csv') as csvfile:
 	for row in csv.reader(csvfile, delimiter="\t"):
 		if len(row) != 4:
 			continue
-		drums[row[0]] = drum(row[1], row[2], row[3], row[0])
+		drums[row[0]] = (row[1], row[2], row[3], row[0])  # ('code', 'note', 'title', 'pattern')
 	with open("GM.py", "w") as fout:
-		fout.write("from collections import namedtuple\n")
-		fout.write('drum = namedtuple("drum", ["code", "note", "title", "pattern"])' + "\n\n")
 		fout.write("drums = ")
-		fout.write(str(drums))
+		fout.write(repr(drums))
